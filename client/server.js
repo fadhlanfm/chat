248b77ea556e58334e91588d10e861b093b2a562
Fadhlan Fariz Makarim <fadhlanfariz95@gmail.com>
2021-11-23 15:23:31 +0700

    first commit

---
+++ client/server.js
@@ -0,0 +1,31 @@
+import { io } from "socket.io-client"
+
+const joinRoomButton = document.getElementById("room-button")
+const messageInput = document.getElementById("message-input")
+const roomInput = document.getElementById("room-input")
+const form = document.getElementById("form")
+
+
+form.addEventListener("submit", event => {
+  event.preventDefault()
+  const message = messageInput.value
+  const room = roomInput.value
+  
+  if (message === "") {
+    return
+  }
+
+  displayMessage(message)
+
+  messageInput.value = ""
+})
+
+joinRoomButton.addEventListener("click", event => {
+  const room = roomInput.value
+})
+
+function displayMessage(message) {
+  const div = document.createElement("div")
+  div.textContent = message
+  document.getElementById("message-container").append(div)
+}
